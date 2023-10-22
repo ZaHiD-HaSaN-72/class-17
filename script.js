3a9dabcceb3693b358ff5700e2b82351cf9aa9f5
ZaHiD-HaSaN-72 <jahidhasan19369@gmail.com>
2023-10-22 17:10:48 +0600

    modal update

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ function countryInfo(){
     };
     
     let mainContainer = document.getElementById('main-box');
+   
+
+  
     function continentsData(allData){
         for (let data of allData){
             
@@ -25,13 +28,27 @@ function countryInfo(){
              <h2>Capital: ${data.capital}</h2>
              <h2>Population: ${data.population}</h2>
              <h2>Country-code: ${data.cca2}</h2>
-             <button  class="btn">See more</button>  
+             <button  class="btn">See more</button> 
+             <div class="additional-data" style="display: none;">
+             <h1 class="flags">${data.name.common}: &nbsp;<img src="${data.flags.png}" alt="country-flag"></h1>
+             <h2>Status: ${data.status}</h2>
+             <h2>Area: ${data.area} square kilometers</h2>
+             <h2>Officilal Name: ${data.name.official}</h2>
+             <h2>Region: ${data.region}</h2>
+             <h2>Subregion: ${data.subregion}</h2>
+             <h2>Car:- Side-${data.car.side}, Sign-${data.car.signs}</h2>
+             <h2>Time Zone: ${data.timezones[0]}</h2>
+             <h2>Week Start: ${data.startOfWeek}</h2>
+             
+         </div> 
             `;
             mainContainer.appendChild(textBox);
 
-        
+      
+
+
         };
-        
+
     };
 
    
@@ -61,30 +78,21 @@ document.addEventListener('click', function (event) {
     if (event.target && event.target.classList.contains('btn')) {
       // Get the modal and modal content elements
       const modal = document.getElementById('myModal');
-      const modalContent = document.getElementById('modal-content'); 
-      fetch(ApiUrl)
-        .then(response => response.json())
-        .then(data => {
-          // Add the additional data to the modal
-          const country = data.find((c) => c.continents);
-          
-          modalContent.innerHTML = `
-          <h1 class="flags">${country.name.common}: &nbsp;<img src="${country.flags.png}" alt="country-flag"></h1>
-            <h2>Status: ${country.status}</h2>
-            <h2>Area: ${country.area} square kilometers</h2>
-            <h2>Officilal Name: ${country.name.official}</h2>
-            <h2>Region: ${country.region}</h2>
-            <h2>Subregion: ${country.subregion}</h2>
-            <h2>Language: ${country.languages[0]}</h2>
-            <h2>Car:- Side-${country.car.side}, Sign-${country.car.signs}</h2>
-            <h2>Time Zone: ${country.timezones[0]}</h2>
-            <h2>Week Start: ${country.startOfWeek}</h2>
-            <h2>PostalCode: ${country.postalCode.format}</h2>
-           
-          `;
-        });
+      const modalContent = document.getElementById('modal-content');
+  
+      // Get the data for the specific country
+      const countryData = event.target.parentNode;
+  
+      // Extract the additional data
+      const additionalData = countryData.querySelector('.additional-data');
+    //   additionalData.style.display='block';
+  
+      // Create the modal content
+      modalContent.innerHTML = additionalData.innerHTML;
+  
       // Show the modal
       modal.style.display = 'block';
+  
       // Add an event listener to close the modal when the close button is clicked
       const closeBtn = document.getElementsByClassName('close')[0];
       closeBtn.addEventListener('click', function () {
@@ -92,9 +100,14 @@ document.addEventListener('click', function (event) {
       });
     }
   });
+  
+  
+  
 // Mosal end
 
 });
 };
 
 countryInfo();
+
+ 
